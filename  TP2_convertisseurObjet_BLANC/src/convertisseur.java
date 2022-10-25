1fa162f6b74a453b0834afe460be7b89bc56db61
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-10-25 17:15:36 +0200

    TP2

---  TP2_convertisseurObjet_BLANC/src/convertisseur.java
+++  TP2_convertisseurObjet_BLANC/src/convertisseur.java
@@ -8,61 +8,67 @@
  * @author User
  */
 public class convertisseur {
-    public convertisseur() {
-        nbConversions=0;
+    
+    public convertisseur () {
+        nbConversions = 0 ;
     }
     int nbConversions;
-    private static double CelciusVersKelvin(double celcius){
+    
+    public double CelciusVersKelvin(double celcius){
         
         double Kelvin = celcius + 273.15;
-        
+        nbConversions=nbConversions+1;
         return Kelvin;
         
     }
     
     
-    private static double KelvinVersCelcius(double kelvin){
+    public double KelvinVersCelcius(double kelvin){
 
         double celcius = kelvin - 273.15;
-
+        nbConversions=nbConversions+1;
         return celcius;
         
     }
     
     
-    private static double FarenheitVersCelcius(double farenheit){
+    public double FarenheitVersCelcius(double farenheit){
 
         double celcius = (farenheit - 32) / 1.8;
-
+        nbConversions=nbConversions+1;
         return celcius;
 
     }
 
 
-    private static double CelciusVersFarenheit(double celcius){
+    public double CelciusVersFarenheit(double celcius){
 
         double farenheit = (celcius * 1.8) + 32;
-
+        nbConversions=nbConversions+1;
         return farenheit;
         
     }
     
     
-    private static double KelvinVersFarenheit(double kelvin){
+    public double KelvinVersFarenheit(double kelvin){
 
         double farenheit = CelciusVersFarenheit(KelvinVersCelcius(kelvin));
-
+        nbConversions=nbConversions+1;
         return farenheit;
         
     }
     
     
-    private static double FarenheitVersKelvin(double farenheit){
+    public  double FarenheitVersKelvin(double farenheit){
 
         double kelvin = CelciusVersKelvin(FarenheitVersCelcius(farenheit));
-
+        nbConversions=nbConversions+1;
         return kelvin;
         
+    }
+    
+    public String toString () {
+        return "nb de conversions"+ nbConversions;
     }
     
 }
---
+++  TP2_convertisseurObjet_BLANC/src/tp2_convertisseurobjet_blanc/TP2_convertisseurObjet_BLANC.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_blanc;
+
+/**
+ *
+ * @author User
+ */
+public class TP2_convertisseurObjet_BLANC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        convertisseur temp1 = new convertisseur();
+        convertisseur temp2 = new convertisseur();
+        double temp = 1.0;
+        convertisseur.FarenheitVersCelcius(temp);
+        convertisseur.CelciusVersKelvin(temp);
+        
+        System.out.println()
+    }
+    
+}
