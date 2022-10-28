f1b240f8e90137c3baa1e63c674a34b95efd820e
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-10-28 16:58:39 +0200

    TP2 Exo4 mise de l'attribut personne a partir de la classe personne

--- TP2_relation_1_BLANC/src/tp2_relation_1_blanc/Voiture.java
+++ TP2_relation_1_BLANC/src/tp2_relation_1_blanc/Voiture.java
@@ -10,16 +10,19 @@ package tp2_relation_1_blanc;
  */
 public class Voiture {
     
-    public Voiture(String modele,String Marque,int PuissanceCV) {
+    public Voiture(String modele,String Marque,int PuissanceCV,Personne Propriétaire) {
     modele1=modele;
     Marque1=Marque;
     PuissanceCV1=PuissanceCV;
+    Personne1=Propriétaire;
     }
     String modele1;
     String Marque1;
     int PuissanceCV1;
+    Personne Personne1;
     @Override
     public String toString () {
-        return ("Voiture de modele " + modele1 + " de marque " + Marque1 + " et de Puissance en CV de " + PuissanceCV1);
+        return ("Voiture de modele " + modele1 + " de marque " + Marque1 + " et de Puissance en CV de " + PuissanceCV1 + " et son propriètaire est " + Personne1);
     }
+    // Créer un type d'affichage qui donne le modle, la marque, la puissance et le propriétaire
 }
