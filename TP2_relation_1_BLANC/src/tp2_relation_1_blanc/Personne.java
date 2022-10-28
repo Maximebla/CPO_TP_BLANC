435dfa31d65a2adaec7b8dfa3eed464c8bdb2c1e
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-10-28 17:15:59 +0200

    TP2 Exo4 fin de séance vendredi

--- TP2_relation_1_BLANC/src/tp2_relation_1_blanc/Personne.java
+++ TP2_relation_1_BLANC/src/tp2_relation_1_blanc/Personne.java
@@ -9,12 +9,17 @@ package tp2_relation_1_blanc;
  * @author User
  */
 public class Personne {
-    public Personne(String nom,String prenom) {
+    public Personne(String nom,String prenom,int nbvoitures, Voiture [] liste_voitures) {
     nom1=nom;
-    prenom1=prenom;        
+    prenom1=prenom;
+    nbvoitures1 = nbvoitures;
+    liste_voitures1 = liste_voitures;
     }
     String nom1;
     String prenom1;
+    int nbvoitures1;
+    Voiture[] liste_voitures1;
+    
     @Override
     public String toString () {
         return ("Le nom de la personne est " + nom1 + " et son prenom est " + prenom1);
