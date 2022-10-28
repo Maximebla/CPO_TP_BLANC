71f2f705b8cbad47a9673c46184c5bb401132d82
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-10-28 15:21:10 +0200

    TP2 Exo3 référence objet peut elle réferencer un autre objet?

--- TP2_manip_BLANC/src/tp2_manip_blanc/TP2_manip_BLANC.java
+++ TP2_manip_BLANC/src/tp2_manip_blanc/TP2_manip_BLANC.java
@@ -23,6 +23,17 @@ public class TP2_manip_BLANC {
         // 2 assiettes crées et l'assiette3 vaut la même tartiflette que l'assiette2
         System.out.println("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;
         System.out.println("nb de calories de Assiette 3 : " +assiette3.nbCalories) ;
+        
+        assiette2=assiette1;
+        assiette1=assiette3;
+        
+        
+        //Moussaka assiette666 = assiette1 ;
+        //Moussaka assiette667 = new Tartiflette() ;
+        // Aucune n'est correct
+        // une référence objet qui annonce référencer un type d’objet peut-elle en référencer un autre qui n’a aucun rapport ?
+        // Non
+        
     }
-    
+        
 }
