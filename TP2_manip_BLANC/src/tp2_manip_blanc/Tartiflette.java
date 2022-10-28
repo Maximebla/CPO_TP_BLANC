3bb0c750a542bea03f7938e44c4777f890df85c9
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-10-28 15:28:20 +0200

    TP2 Exo3

---
+++ TP2_manip_BLANC/src/tp2_manip_blanc/Tartiflette.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_blanc;
+
+/**
+ *
+ * @author User
+ */
+public class Tartiflette {
+    int nbCalories;
+    public Tartiflette(int Calories){
+        nbCalories=Calories;
+    }
+    
+}
