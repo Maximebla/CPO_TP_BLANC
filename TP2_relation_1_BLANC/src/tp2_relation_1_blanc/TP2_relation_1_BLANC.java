49d10bf15c674b724bdc6cf452f2c7d873d5ac89
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-10-29 11:53:30 +0200

    TP2 Exo4(Q9) 1er et 2eme voiture de bob et reno

--- TP2_relation_1_BLANC/src/tp2_relation_1_blanc/TP2_relation_1_BLANC.java
+++ TP2_relation_1_BLANC/src/tp2_relation_1_blanc/TP2_relation_1_BLANC.java
@@ -25,15 +25,26 @@ public class TP2_relation_1_BLANC {
         Personne bob = new Personne("Bobby", "Sixkiller");
         Personne reno = new Personne("Reno", "Raines");
         
-        System.out.println("combien aez-vous de voitures");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        Voiture [] liste_voitures = new Voiture[]; 
-        
-        for (int i=0; i < 3; i++) {
-            tabEntiers [i] = new Voiture(i) ;
-        }
         System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        
+        bob.liste_voitures[0] = uneClio ;
+        bob.liste_voitures[1] = uneAutreClio ;
+        bob.nbVoitures = 2 ;
+        uneClio.Proprietaire = bob ;
+        uneAutreClio.Proprietaire = bob ;
+        
+        System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ;
+        System.out.println("la deuxième voiture de Bob est " +bob.liste_voitures[1] ) ;
+        
+        
+        reno.liste_voitures[0] = une2008 ;
+        reno.liste_voitures[1] = uneMicra ;
+        reno.nbVoitures = 2 ;
+        une2008.Proprietaire = reno ;
+        uneMicra.Proprietaire = reno ;
+        
+        System.out.println("la premiere voiture de Bob est " +reno.liste_voitures[0] ) ;
+        System.out.println("la deuxième voiture de Bob est " +reno.liste_voitures[1] ) ;
     }
     
 }
