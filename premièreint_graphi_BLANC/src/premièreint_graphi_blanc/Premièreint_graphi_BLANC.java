54951e435c4122e4a449fe0a87d9a644caa195a1
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-11-07 12:35:16 +0100

    début interface graphique(prof)

--- premièreint_graphi_BLANC/src/premièreint_graphi_blanc/Premièreint_graphi_BLANC.java
+++ premièreint_graphi_BLANC/src/premièreint_graphi_blanc/Premièreint_graphi_BLANC.java
@@ -13,8 +13,5 @@ public class Premièreint_graphi_BLANC {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+
 }
