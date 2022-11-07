00bc6ca2dd0ac43da80fe9aed56d7b696dbd1667
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-11-07 11:38:33 +0100

    début interface graphique

---
+++ premièreint_graphi_BLANC/src/premièreint_graphi_blanc/Premièreint_graphi_BLANC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package premièreint_graphi_blanc;
+
+/**
+ *
+ * @author User
+ */
+public class Premièreint_graphi_BLANC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
