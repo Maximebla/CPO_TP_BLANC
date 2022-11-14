80d37781fae03d5d33c790289c0945fca48f7b54
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-11-14 10:05:04 +0100

    TP3 classe Arme

---
+++ TP3_BLANC_MAXIME/src/tp3_blanc_maxime/Arme.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_blanc_maxime;
+
+/**
+ *
+ * @author User
+ */
+public class Arme {
+    String nom;
+    private int nvAtta;
+    
+    public Arme(String Unnom,int UnnvAtta) {
+        nom=Unnom;
+        
+        if ( UnnvAtta >0 &&  UnnvAtta <100){
+            nvAtta=UnnvAtta;
+        }if ( UnnvAtta >100){
+            nvAtta=100;
+        }if (UnnvAtta<0){
+            nvAtta = 1;
+        }
+        
+    }
+
+    public int getNvAtta() {
+        return nvAtta;
+    }
+
+    public void setNvAtta(int nvAtta) {
+        this.nvAtta = nvAtta;
+    }
+
+    
+    public String toString() {
+        return "l'arme s'appelle " + nom + "et a une attaque de " + nvAtta + "."; 
+    }    
+}
