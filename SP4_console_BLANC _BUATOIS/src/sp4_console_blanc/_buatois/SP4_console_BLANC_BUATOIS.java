8b53231be8c4c924557c608026b07a6ea6c42eac
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-11-18 14:21:31 +0100

    SP4 creation du fichier

---
+++ SP4_console_BLANC _BUATOIS/src/sp4_console_blanc/_buatois/SP4_console_BLANC_BUATOIS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_blanc._buatois;
+
+/**
+ *
+ * @author User
+ */
+public class SP4_console_BLANC_BUATOIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
