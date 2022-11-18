28e78b54e68285268b7c4eb20e4a9f2cc83b14c0
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-11-18 17:13:57 +0100

    SP4 classe fin classe Joueur et début classe Plateau de jeu

--- SP4_console_BLANC _BUATOIS/src/sp4_console_blanc/_buatois/Joueur.java
+++ SP4_console_BLANC _BUATOIS/src/sp4_console_blanc/_buatois/Joueur.java
@@ -4,10 +4,43 @@
  */
 package sp4_console_blanc._buatois;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author User
  */
 public class Joueur {
+    private String nom;
+    Joueur Couleur;
+    ArrayList<Jeton> reserveJetons = new ArrayList<Jeton>();
+    int nombreJetonsRestants = reserveJetons.size();
+    int nombreDesintegrateurs;
+
+    public Joueur(String nom) {
+        this.nom = nom;
+    }
+
+    public void affecterCouleur(Joueur Couleur) {
+        this.Couleur = Couleur;
+    }
     
+    public void AjouterJeton(Jeton J){
+        reserveJetons.add(J);
+    }
+    
+    public void obtenirDesintegrateur(){
+        nombreDesintegrateurs += 1;
+    }
+    
+    public boolean utiliserDesintegrateur(){
+        if (nombreDesintegrateurs ==0){
+            return true;
+        }
+        else{
+            nombreDesintegrateurs -= 1;
+            return false;
+        }
+        
+    }
 }
--- SP4_console_BLANC _BUATOIS/src/sp4_console_blanc/_buatois/PlateauDeJeu.java
+++ SP4_console_BLANC _BUATOIS/src/sp4_console_blanc/_buatois/PlateauDeJeu.java
@@ -9,5 +9,66 @@ package sp4_console_blanc._buatois;
  * @author User
  */
 public class PlateauDeJeu {
+    CelluleDeGrille [][] grille = new CelluleDeGrille [6][7];
     
+    public void PlateauDeJeu(){
+        for (int l=0; l<6; l++){
+            for (int c=0; c<7; c++){
+                grille[l][c] = new CelluleDeGrille();
+            }
+        }
+    }
+    
+    public int ajouterJetonDansColonne(Jeton J, int c){
+        int a=- 1;
+        for (int l=0; l<7; l++){
+            if ( grille[l][c].presenceJeton() == false){
+                grille[l][c].affecterJeton(J);
+                a = l;
+            }   
+        }  
+    return a;
+    }
+    
+    public boolean grilleRemplie(){
+        boolean r=false;
+        for (int c=0; c<6; c++){
+            if (grille[7][c].presenceJeton() == true){
+                r=true;
+            }
+        }
+    return r;  
+    }
+    
+    public viderGrille(Joueur ;Joueur){
+        
+    }
+    
+    public afficherGrilleSurConsole(){
+        
+    }
+    
+    public presenceJeton(int x, int y){
+        
+    }
+    
+    public ireCouleurDuJeton(int x,int y){
+        
+    }
+    
+    public etreGagnantePourCouleur(String){
+        
+    }
+    
+    public tasserLigne(int){
+        
+    }
+    
+    public colonneRemplie(){
+        
+    }
+    
+    public placerTrouNoir(){
+        
+    }
 }
