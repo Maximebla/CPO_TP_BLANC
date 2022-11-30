71a8b50aa49df57fe686a7c7473454c38f2d33d7
Maxime Blanc <maxime.blanc@epfedu.fr>
2022-11-30 14:26:44 +0100

    SP4 création classe partie, et des attributs

--- SP4_console_BLANC _BUATOIS/src/sp4_console_blanc/_buatois/Partie.java
+++ SP4_console_BLANC _BUATOIS/src/sp4_console_blanc/_buatois/Partie.java
@@ -9,5 +9,25 @@ package sp4_console_blanc._buatois;
  * @author User
  */
 public class Partie {
+    private Joueur [] listeJoueurs = new Joueur[2];
+    private Joueur joueurCourant;
+    private PlateauDeJeu plateau;
+
+    public Partie(Joueur J1, Joueur J2) {
+        listeJoueurs[1]=J1;
+        listeJoueurs[2]=J2;
+    }
     
+    public attribuerCouleurAuxJoueurs(){
+        Math.random()*(2-1);
+           if (a==1){
+               
+           }else{
+               
+           }
+    }
+    
+    public creerEtAffecterJeton(Joueur J){
+        
+    }
 }
